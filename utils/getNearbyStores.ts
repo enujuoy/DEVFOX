ae53c927df60209411e4d3867a0751dda7f577d7
enujuoy <yjrio14@gmail.com>
2025-04-25 14:03:32 +0900

    distanceChange
    distanceChange

--- utils/getNearbyStores.ts
+++ utils/getNearbyStores.ts
@@ -1,20 +1,22 @@
+// 🔹 utils/getNearbyStores.ts
 import Constants from 'expo-constants';
 
-export const getNearbyStores = async (lat: number, lon: number) => {
+export const getNearbyStores = async (lat: number, lon: number, radius: number = 1000) => {
   const apiKey = Constants.expoConfig?.extra?.googleMapsApiKey;
-  const radius = 1000;
 
   const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lon}&radius=${radius}&type=convenience_store&language=ja&key=${apiKey}`;
-
   const response = await fetch(url);
   const json = await response.json();
 
   if (!json.results) return [];
 
-  return json.results.map((place: any) => ({
-    name: place.name,
-    latitude: place.geometry.location.lat,
-    longitude: place.geometry.location.lng,
-    address: place.vicinity,
-  }));
+  return json.results
+    .filter((place: any) => place.geometry?.location?.lat && place.geometry?.location?.lng)
+    .map((place: any) => ({
+      placeId: place.place_id,
+      name: place.name,
+      address: place.vicinity,
+      latitude: place.geometry.location.lat,
+      longitude: place.geometry.location.lng,
+    }));
 };
